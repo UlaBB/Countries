593f88a26409885af4d53d333150f992aff1bbbb
Ula Buczak <mankowskau@gmail.com>
2020-11-07 18:23:46 +0100

    Add navigation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MainLayout from './presentational/main-layout.component';
+import Home from './presentational/home.component';
+import Contact from './presentational/contact.component';
+import NotFound from './presentational/not-found.component';
 import store from './store';
+// import styles from '../styles/style.scss';
 
-render(
+ReactDOM.render(
   <Provider store={store}>
-    <h1>Hello world!</h1>
+    <BrowserRouter>
+      <MainLayout>
+        <Switch location={location} >
+          <Route exact path='/' component={Home} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/contact' component={Contact} />
+          <Route path='*' component={NotFound} />
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('app')
 );
-
